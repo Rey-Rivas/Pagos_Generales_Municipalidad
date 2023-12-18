e8f11b9845ffe1ba21ea7c08f6e3334d58a2412e
Reynaldo Rivas Oliva <reynaldo.rivas2001@alumnos.ubiobio.cl>
2023-12-18 01:23:48 -0300

    Casi listo falta subir el archivo y filtrar las deudas

--- frontend/src/components/CrearApelacionComp.vue
+++ frontend/src/components/CrearApelacionComp.vue
@@ -18,15 +18,16 @@
                     <v-combobox
                         v-model="datosApelacion.deudaSel"
                         label="Deudas"
+                        :items="deudaList.map(deuda => deuda.descripcion)"
                         required
                     ></v-combobox>
                 </v-col>
 
                 <v-col cols="12" md="5">
                     <v-combobox
+                        v-model="datosApelacion.estado"
                         label="Estado"
-                        disabled="true"
-                        :items="['Pendiente', 'Pendiente Justificado', 'Aprobado', 'Rechazado', 'Pagado', 'Fuera de plazo']"
+                        :items="['Pendiente']"
                         required
                     ></v-combobox>
                 </v-col>
@@ -58,7 +59,7 @@ import fetchBase from '@/services/fetch'
                 descripcion: '',
                 deudaSel: '',
                 estado: 'Pendiente',
-                documento: '',
+                documento: null,
              },        
             valid: false,
 
@@ -79,13 +80,30 @@ import fetchBase from '@/services/fetch'
 
     }),
     methods: {
+        async uploadFile(event) {
+            const file = event.target.files[0];
+            const formData = new FormData();
+            formData.append('file', file);
+
+            try {
+                const response = await fetchBase('/upload.route', {
+                method: 'POST',
+                body: formData,
+                });
+
+                console.log('File uploaded:', response);
+            } catch (error) {
+                console.error('Error uploading file:', error);
+            }
+        },
+
         async subirApelacion(){
             try{
                 const deuda = this.deudaList.find(deuda => deuda.descripcion === this.datosApelacion.deudaSel);
 
                 console.log({
                     descripcion: this.datosApelacion.descripcion,
-                    deudaID: deuda ? deuda.deudaID : null,
+                    deudaID: deuda ? deuda._id : null,
                     estado: this.datosApelacion.estado,
                     documento: this.datosApelacion.documento,
                     RUTUsuario: this.user_RUT,
@@ -98,7 +116,7 @@ import fetchBase from '@/services/fetch'
                     },
                     body: JSON.stringify({
                         descripcion: this.datosApelacion.descripcion,
-                        deudaID: deuda ? deuda.deudaID : null,
+                        deudaID: deuda ? deuda._id : null,
                         estado: this.datosApelacion.estado.toLocaleLowerCase(),
                         documento: this.datosApelacion.documento,
                         RUTUsuario: this.user_RUT,
@@ -128,15 +146,15 @@ import fetchBase from '@/services/fetch'
 
                 this.deudaList = [];
                 for (let deuda of deudaList.data) {
-                    const deudaData = await fetchBase(`/deudas/usuario/${this.rut}`, {
+                    const deudaData = await fetchBase(`/deudas/usuario/${this.user_RUT}`, {
                         headers: {
                             'Authorization': 'Bearer ' + this.token,
                         },
                     });
-                    if (deudaData.data.RUT === this.rut) {
+                    if (deudaData.data.RUT === this.user_RUT) {
                         this.deudaList.push(deuda);
                     }
-                }
+                };
                 console.log('Deudas:', deudaList.data);
             }catch(error){
                 console.log('Error al obtener las deudas',error);
@@ -148,7 +166,7 @@ import fetchBase from '@/services/fetch'
 
             console.log({
                     descripcion: this.datosApelacion.descripcion,
-                    deudaID: deuda ? deuda.deudaID : null,
+                    deudaID: deuda ? deuda._id : null,
                     estado: this.datosApelacion.estado,
                     documento: this.datosApelacion.documento,
                     RUTUsuario: this.user_RUT,
@@ -156,7 +174,19 @@ import fetchBase from '@/services/fetch'
         }
     },
     created() {
-        this.getDeudas();
+        //this.getDeudas();
+    },
+    computed: {
+        deudaSelComputed: {
+            get() {
+                const deuda = this.deudaList.find(deuda => deuda.descripcion === this.datosApelacion.deudaSel);
+                return deuda ? `${deuda.descripcion}` : '';
+            },
+            set(value) {
+                const descripcion = value.split(' ').pop().slice(1, -1); // Extracts the RUT from the string
+                this.datosApelacion.deudaSel = descripcion;
+            }
+        }
     },
 }
 </script>
